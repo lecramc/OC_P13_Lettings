6bfcae12b7f123a2012b29a16c1a93f689db498c
Clement MARCEL <clmtmarcel@gmail.com>
2023-03-07 08:45:47 +0100

    remove unecessary var

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -111,11 +111,3 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-]
-
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
