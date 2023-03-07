79182e1ffdaf6e81ed79a5f5822387895e6c3245
Clement MARCEL <clmtmarcel@gmail.com>
2023-03-07 11:18:34 +0100

    add sentry

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 
 from . import views
 
+
 def trigger_error(request):
-    division_by_zero = 1 / 0
+    return 1 / 0
+
 
 urlpatterns = [
     path('', views.index, name='index'),
