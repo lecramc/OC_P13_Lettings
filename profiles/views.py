ea999f6c9ef22b8d82497ae396d7c55087b2792f
Clement MARCEL <lecramproject@gmail.com>
2023-03-13 16:16:23 +0100

    FIX can't acceed to template

--- profiles/views.py
+++ profiles/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import render
 def index(request):
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
-    return render(request, 'index.html', context)
+    return render(request, 'profiles/index.html', context)
 
 # Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac
 # laoreet neque quis, pellentesque dui. Nullam facilisis pharetra vulputate.
@@ -22,4 +22,4 @@ def index(request):
 def profile(request, username):
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
-    return render(request, 'profile.html', context)
+    return render(request, 'profiles/profile.html', context)
