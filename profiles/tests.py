dac4d463a67b9854ab60c3be538e53765fb5d8c2
Clement MARCEL <lecramproject@gmail.com>
2023-03-13 16:21:03 +0100

    mark as db test

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,14 +1,15 @@
+import pytest
 from django.test import Client
 from django.urls import reverse
 
-
+@pytest.mark.django_db
 def test_view_code():
     """Check if the view is accessible"""
     client = Client()
     response = client.get(reverse('profiles:index'))
     assert response.status_code == 200
 
-
+@pytest.mark.django_db
 def test_view_title():
     """Check if the title is OK"""
     client = Client()
