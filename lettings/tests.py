c2babea6a9d73bb5a670bf7b33a6aa786b953814
Clement MARCEL <lecramproject@gmail.com>
2023-03-13 17:26:07 +0100

    solve flake8 error

--- lettings/tests.py
+++ lettings/tests.py
@@ -2,11 +2,13 @@ import pytest
 from django.test import Client
 from django.urls import reverse
 
+
 @pytest.mark.django_db
 def test_view_code():
     client = Client()
     response = client.get(reverse('lettings:index'))
     assert response.status_code == 200
+
 
 @pytest.mark.django_db
 def test_view_title():
--- profiles/tests.py
+++ profiles/tests.py
@@ -2,12 +2,14 @@ import pytest
 from django.test import Client
 from django.urls import reverse
 
+
 @pytest.mark.django_db
 def test_view_code():
     """Check if the view is accessible"""
     client = Client()
     response = client.get(reverse('profiles:index'))
     assert response.status_code == 200
+
 
 @pytest.mark.django_db
 def test_view_title():
