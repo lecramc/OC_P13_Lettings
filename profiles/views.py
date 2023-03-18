5808b75ecbd8aede94fd4011d8ffd6959f492365
Clement MARCEL <lecramproject@gmail.com>
2023-03-18 11:22:09 +0100

    add backslash

--- profiles/views.py
+++ profiles/views.py
@@ -23,3 +23,5 @@ def profile(request, username):
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
     return render(request, 'profiles/profile.html', context)
+
+
